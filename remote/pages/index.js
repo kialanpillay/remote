4f12863ce8c24f29ea6172bac4ddbba90d5c6963
Kialan Pillay <kialanpillay@gmail.com>
2020-07-13 13:54:35 +0200

    Home Page

--- remote/pages/index.js
+++ remote/pages/index.js
@@ -1,48 +1,51 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Icon from '@material-ui/core/';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Remote</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Remote <Icon style={{ fontSize: 64 }}>settings_remote</Icon>
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          A hub for essential information.
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>COVID-19 &rarr;</h3>
+            <p>Find the latest statistics about COVID-19.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/markets" className="card">
+            <h3>Financial Markets &rarr;</h3>
+            <p>View detailed market data from TradingView</p>
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/news"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>News &rarr;</h3>
+            <p>Discover top headlines from around the world.</p>
+          </a>
+
+          <a
+            href="/weather"
+            className="card"
+          >
+            <h3>Weather &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Check the local forecast for the next few days.
             </p>
           </a>
         </div>
@@ -50,12 +53,11 @@ export default function Home() {
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.kialanpillay.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          &copy; Kialan Pillay. Remote is a tool to boost your productivity whilst working from home. 
         </a>
       </footer>
 
@@ -67,6 +69,7 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          background-color: white;
         }
 
         main {
@@ -157,6 +160,7 @@ export default function Home() {
           text-decoration: none;
           border: 1px solid #eaeaea;
           border-radius: 10px;
+          background-color: white;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
 
