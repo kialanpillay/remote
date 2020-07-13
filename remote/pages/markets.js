7edba1a0147782dd9c83c047b6cbbba0760cec50
Kialan Pillay <kialanpillay@gmail.com>
2020-07-13 18:41:05 +0200

    Financial Page

---
+++ remote/pages/markets.js
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import Head from "next/head";
+import Chart from "../components/Chart";
+
+function Markets({ data }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Remote - Market</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="title">FINANCIAL MARKETS</h1>
+        <div className="data">
+          <Chart />
+        </div>
+        <h2>
+            <Link href="/">
+              <a> &larr; Back Home</a>
+            </Link>
+          </h2>
+      </main>
+
+      <style jsx>{`
+        .container {
+          max-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 0rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title {
+          margin: 3rem;
+          line-height: 1.15;
+          font-size: 4rem;
+          letter-spacing: 0.5rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        .data {
+            margin-top: 2rem;
+            flex-basis: 45%;
+            align-items: center;
+            width: 60%;
+          }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Markets;
