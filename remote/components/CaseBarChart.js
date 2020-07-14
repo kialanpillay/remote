d2abf681ce3b75dc183752bd412c64c4a91303f6
Kialan Pillay <kialanpillay@gmail.com>
2020-07-14 18:35:22 +0200

    Bar Chart Data Update

--- remote/components/CaseBarChart.js
+++ remote/components/CaseBarChart.js
@@ -5,12 +5,15 @@ export default class Example extends React.PureComponent {
   render() {
     const data = [
       {
-        active: this.props.data.confirmed - this.props.data.recovered,
-        recovered: this.props.data.recovered,
+        name: 'Percentage',
+        active: (((this.props.data.confirmed - this.props.data.recovered) / this.props.data.confirmed) * 100).toFixed(2),
+        recovered: ((this.props.data.recovered / this.props.data.confirmed) * 100).toFixed(2)
       },
     ];
     return (
       <BarChart width={180} height={220} data={data}>
+        <Tooltip cursor={false}/>
+        <XAxis hide={true} dataKey="name" />
         <Bar
           dataKey="recovered"
           fill="green"
